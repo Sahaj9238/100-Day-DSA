9dc019dd9bd26b01d7a5770b487015f41ef31910
Sahaj9238 <sahajparikh2007@gmail.com>
2026-02-01 22:55:52 +0530

    Create day1q1.c

---
+++ day1/day1q1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n,p,e;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        printf("Enter the element %d:",i+1);
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the position to insert:");
+    scanf("%d",&p);
+    printf("Enter the element to insert:");
+    scanf("%d",&e);
+    if(p<1||p>n){
+        printf("Invalid Position");
+    }
+    n=n+1;
+    for(int i=n-1;i>=p;i--){
+        a[i]=a[i-1];
+    }
+    a[p-1]=e;
+    printf("After inserting the element:");
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
