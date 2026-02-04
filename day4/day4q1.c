a8ea46658fc57d3685351703c35e18f643c17dbf
Sahaj9238 <sahajparikh2007@gmail.com>
2026-02-04 21:16:05 +0530

    Create day4q1.c

---
+++ day4/day4q1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++) {
+        scanf("%d",&a[i]);
+    }
+    int left=0, right=n-1;
+    while(left<right){
+        int temp=a[left];
+        a[left]=a[right];
+        a[right]=temp;
+        left++;
+        right--;
+    }
+    for(int i=0;i<n;i++) {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
