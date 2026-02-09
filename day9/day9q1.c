cbaa7689cf8298ba5174ae6a2bb8c7fa4c83415a
Sahaj9238 <sahajparikh2007@gmail.com>
2026-02-09 23:15:36 +0530

    Create day9q1.c

---
+++ day9/day9q1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char s[1000];
+    printf("Enter the string:");
+    scanf("%s",s);
+    int left=0;
+    int right=strlen(s)-1;
+    while (left < right){
+        char temp=s[left];
+        s[left]=s[right];
+        s[right]=temp;
+        left++;
+        right--;
+    }
+    printf("%s",s);
+    return 0;
+}
