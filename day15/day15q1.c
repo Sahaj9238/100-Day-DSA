7b4bd82476df09a4d4199b7022621501a6bee786
Sahaj9238 <sahajparikh2007@gmail.com>
2026-02-16 21:40:56 +0530

    Create day15q1.c

---
+++ day15/day15q1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int m, n;
+    printf("Enter the number of rows and columns:");
+    scanf("%d %d", &m, &n);
+    int a[m][n];
+    int sum=0;
+    for(int i=0;i<m;i++) {
+        for(int j=0;j<n;j++) {
+            printf("Enter the element:");
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(int i=0;i<m&&i<n;i++) {
+        sum += a[i][i];
+    }
+    printf("%d", sum);
+    return 0;
+}
