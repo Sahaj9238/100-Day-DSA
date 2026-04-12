9f3939fda0e19aed1fb865933530b706ddea5521
Sahaj9238 <sahajparikh2007@gmail.com>
2026-04-12 16:42:39 +0530

    Create day47q1.c

---
+++ day47/day47q1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    if (data == -1) return NULL;
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0) return NULL;
+
+    struct Node** nodes = (struct Node**)malloc(n * sizeof(struct Node*));
+
+    for (int i = 0; i < n; i++)
+        nodes[i] = createNode(arr[i]);
+
+    int j = 1;
+    for (int i = 0; i < n && j < n; i++) {
+        if (nodes[i] != NULL) {
+            nodes[i]->left = nodes[j++];
+            if (j < n)
+                nodes[i]->right = nodes[j++];
+        }
+    }
+
+    return nodes[0];
+}
+
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+    int left = height(root->left);
+    int right = height(root->right);
+    return (left > right ? left : right) + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    printf("%d\n", height(root));
+
+    return 0;
+}
